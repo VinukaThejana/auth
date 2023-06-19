f5a99571668794b1192c10eccee1442b0d856dce
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-06-19 19:50:42 +0530

    Added the ability to initialize a connection to the postgres database

---
+++ backend/initialize/db.go
@@ -0,0 +1,39 @@
+package initialize
+
+import (
+	"fmt"
+
+	"github.com/VinukaThejana/auth/backend/config"
+	"github.com/fatih/color"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	gormLogger "gorm.io/gorm/logger"
+)
+
+// DB contains the struct to hold the database instance
+type DB struct {
+	DB *gorm.DB
+}
+
+// InitDB initializes a connnection to the postgress database
+func (h *H) InitDB(env *config.Env) {
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Colombo", env.DBHost, env.DBUser, env.DBPassword, env.DBName, fmt.Sprint(env.DBPort))
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Errorf(err, nil)
+	}
+
+	db.Exec("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\"")
+	db.Logger = gormLogger.Default.LogMode(gormLogger.Info)
+
+	color.Blue("Running migrations ... ")
+	err = db.AutoMigrate()
+	if err != nil {
+		errMsg := "Error running migrations !"
+		log.Errorf(err, &errMsg)
+	}
+
+	h.DB = &DB{
+		DB: db,
+	}
+}
--- backend/initialize/initialize.go
+++ backend/initialize/initialize.go
@@ -1,3 +1,11 @@
 // Package initialize is used to initialize connections to third party services
 package initialize
+
+import "github.com/VinukaThejana/go-utils/logger"
+
 var log logger.Logger
+
+// H contains all the connections to third party services
+type H struct {
+	DB *DB
+}
