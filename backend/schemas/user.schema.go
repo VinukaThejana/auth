9ef3b818c4ee570b157c4b9e9aa2ebd76cd8f55a
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-06-20 19:27:07 +0530

    Added the user related schema

---
+++ backend/schemas/user.schema.go
@@ -0,0 +1,62 @@
+package schemas
+
+import (
+	"time"
+
+	"github.com/VinukaThejana/auth/backend/models"
+	"github.com/go-playground/validator/v10"
+	"github.com/google/uuid"
+)
+
+// RegisterInput is a struct that defines what the server expects from the user upon registration
+type RegisterInput struct {
+	Name                 string `json:"name" validate:"required,min=3,max=40"`
+	Email                string `json:"email" validate:"required,email"`
+	Password             string `json:"password" validate:"required,min=8,max=200"`
+	PasswordConfirmation string `json:"password_confirm" validate:"required,min=8,max=200"`
+}
+
+// Validate is a function that is used to vaidate user input upon registration
+func (rI RegisterInput) Validate() (err error) {
+	v := validator.New()
+	err = v.Struct(rI)
+	return err
+}
+
+// LoginInput is a struct that defines what the server expects from the user upon login
+type LoginInput struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=8,max=200"`
+}
+
+// Validate is a function that is used to vaidate user input upon login
+func (lI LoginInput) Vaidate() (err error) {
+	v := validator.New()
+	err = v.Struct(lI)
+	return err
+}
+
+// UserResponse is a struct that contains all the relevant feilds of the models.User when sending the
+// user session to the client side
+type UserResponse struct {
+	ID        uuid.UUID `json:"id,omitempty"`
+	Name      string    `json:"name,omitempty"`
+	Email     string    `json:"email,omitempty"`
+	Role      string    `json:"role,omitempty"`
+	Provider  string    `json:"provider"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+// FilterUserRecord is a funcion that is used to filter the models.User struct to a client freindly manner
+func FilterUserRecord(user *models.User) UserResponse {
+	return UserResponse{
+		ID:        *user.ID,
+		Name:      user.Name,
+		Email:     user.Email,
+		Role:      *user.Role,
+		Provider:  *user.Provider,
+		CreatedAt: *user.CreatedAt,
+		UpdatedAt: *user.UpdatedAt,
+	}
+}
