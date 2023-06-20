07335146852de4b37a00434d18edc93bf4ce5c7a
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-06-20 22:17:14 +0530

    Added the middleware package

---
+++ backend/middleware/middleware.go
@@ -0,0 +1,11 @@
+// Package middleware is a package that contains all the middewares
+package middleware
+
+import (
+	"github.com/VinukaThejana/auth/backend/schemas"
+	"github.com/VinukaThejana/go-utils/logger"
+)
+
+var log logger.Logger
+
+type response schemas.Response
