c54fd133549eaa825356f06f3e6cde975ee44a55
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-06-20 11:03:56 +0530

    Added the user struct that represent the user table in the realtional database

---
+++ backend/models/user.model.go
@@ -0,0 +1,19 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID        *uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primary_key"`
+	Name      string     `gorm:"type:varchar(100);not null"`
+	Email     string     `gorm:"type:varchar(100);uniqueIndex;not null"`
+	Password  string     `gorm:"type:varchar(100);not null"`
+	Role      *string    `gorm:"type:varchar(50);default:'user';not null"`
+	Provider  *string    `gorm:"type:varchar(50);default:'local';not null"`
+	Verified  *bool      `gorm:"not null;default:false"`
+	CreatedAt *time.Time `gorm:"not null;default:now()"`
+	UpdatedAt *time.Time `gorm:"not null;default:now()"`
+}
