c7ddba6afd3fe81d7026e5f6106fcb270404826e
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-06-20 11:06:59 +0530

    Added some needed package level variables

--- backend/schemas/schemas.go
+++ backend/schemas/schemas.go
@@ -1,6 +1,10 @@
 // Package schemas is used to defiend custom schemas that are used throughout the application
 package schemas
 
+import "github.com/VinukaThejana/go-utils/logger"
+
+var log logger.Logger
+
 // Response is a the default response that is being sent to the client
 type Response struct {
 	Status  string `json:"status"`
