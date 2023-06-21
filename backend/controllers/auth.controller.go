0d745bb8861c154bb65baad1d3590c5eaeefe373
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-06-21 23:20:47 +0530

    Added the ability to login the registered user
    This generates an access token and a refresh token for the user

--- backend/controllers/auth.controller.go
+++ backend/controllers/auth.controller.go
@@ -73,3 +73,102 @@ func (Auth) Register(c *fiber.Ctx, h *initialize.H) error {
 	})
 }
 
+// Login is a function that allows the user to login with the email and password
+func (Auth) Login(c *fiber.Ctx, h *initialize.H, env *config.Env) error {
+	var payload *schemas.LoginInput
+	if err := c.BodyParser(&payload); err != nil {
+		log.Error(err, nil)
+		return c.Status(fiber.StatusBadRequest).JSON(response{
+			Status: errors.ErrBadRequest.Error(),
+		})
+	}
+
+	if err := payload.Vaidate(); err != nil {
+		log.Error(err, nil)
+		return c.Status(fiber.StatusBadRequest).JSON(response{
+			Status: errors.ErrBadRequest.Error(),
+		})
+	}
+
+	var user models.User
+	result := h.DB.DB.First(&user, "email = ?", payload.Email)
+	if result.Error != nil {
+		if result.Error != gorm.ErrRecordNotFound {
+			return c.Status(fiber.StatusBadRequest).JSON(response{
+				Status: errors.ErrBadRequest.Error(),
+			})
+		}
+
+		log.Error(result.Error, nil)
+		return c.Status(fiber.StatusInternalServerError).JSON(response{
+			Status: errors.ErrInternalServerError.Error(),
+		})
+	}
+
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(payload.Password))
+	if err != nil {
+		log.Error(err, nil)
+		return c.Status(fiber.StatusUnauthorized).JSON(response{
+			Status: errors.ErrUnauthorized.Error(),
+		})
+	}
+
+	accessTokenDetails, err := utils.Token{}.CreateToken(h, schemas.User{
+		ID:    user.ID.String(),
+		Name:  user.Name,
+		Email: user.Email,
+	}, env.AccessTokenPrivateKey, env.AccessTokenExpires)
+	if err != nil {
+		log.Error(err, nil)
+		return c.Status(fiber.StatusInternalServerError).JSON(response{
+			Status: errors.ErrInternalServerError.Error(),
+		})
+	}
+
+	refreshTokenDetails, err := utils.Token{}.CreateToken(h, schemas.User{
+		ID:    user.ID.String(),
+		Name:  user.Name,
+		Email: user.Email,
+	}, env.RefreshTokenPrivateKey, env.RefreshTokenExpires)
+	if err != nil {
+		log.Error(err, nil)
+		return c.Status(fiber.StatusInternalServerError).JSON(response{
+			Status: errors.ErrInternalServerError.Error(),
+		})
+	}
+
+	c.Cookie(&fiber.Cookie{
+		Name:     "access_token",
+		Value:    *accessTokenDetails.Token,
+		Path:     "/",
+		MaxAge:   env.AccessTokenMaxAge * 60,
+		Secure:   false,
+		HTTPOnly: true,
+		Domain:   "localhost",
+	})
+
+	c.Cookie(&fiber.Cookie{
+		Name:     "refresh_token",
+		Value:    *refreshTokenDetails.Token,
+		Path:     "/",
+		MaxAge:   env.RefreshTokenMaxAge * 60,
+		Secure:   false,
+		HTTPOnly: true,
+		Domain:   "localhost",
+	})
+
+	c.Cookie(&fiber.Cookie{
+		Name:     "logged_in",
+		Value:    "true",
+		Path:     "/",
+		MaxAge:   env.AccessTokenMaxAge * 60,
+		Secure:   false,
+		HTTPOnly: false,
+		Domain:   "localhost",
+	})
+
+	return c.Status(fiber.StatusOK).JSON(response{
+		Status: errors.Okay,
+	})
+}
+
