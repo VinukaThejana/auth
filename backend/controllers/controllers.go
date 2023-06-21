01ce89701a8e19fc81bae941f7f9686895421281
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-06-21 18:13:11 +0530

    added the controllers package

---
+++ backend/controllers/controllers.go
@@ -0,0 +1,11 @@
+// Package controllers contains all the bussiness logic for available routes
+package controllers
+
+import (
+	"github.com/VinukaThejana/auth/backend/schemas"
+	"github.com/VinukaThejana/go-utils/logger"
+)
+
+var log logger.Logger
+
+type response schemas.Response
