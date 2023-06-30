796d63c964d9659c8ae191c2cbfce69a329a7bfd
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-06-30 20:57:21 +0530

    Fixed the date type so that it only allows the unix timestamp

--- backend/models/token.model.go
+++ backend/models/token.model.go
@@ -1,15 +1,13 @@
 package models
 
 import (
-	"time"
-
 	"github.com/google/uuid"
 )
 
 // Sessions is a model that represents the sessions tabel in the database this table is used to
 // store session related data in the database
 type Sessions struct {
-	UserID    *uuid.UUID `gorm:"type:uuid;primary_key"`
-	TokenID   *uuid.UUID `gorm:"type:uuid;primary_key"`
-	ExpiresAt time.Time  `gorm:"not null"`
+	TokenID   uuid.UUID `gorm:"type:uuid;primary_key"`
+	UserID    uuid.UUID `gorm:"type:uuid"`
+	ExpiresAt int64     `gorm:"not null"`
 }
