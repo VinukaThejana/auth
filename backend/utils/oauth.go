b9966a7dcc757a1410dd8bddc8da67037d3b1ef9
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-07-04 15:33:02 +0530

    This funciton allows to fetch the user details from GitHub when the access token is provided

--- backend/utils/oauth.go
+++ backend/utils/oauth.go
@@ -60,3 +60,45 @@ func (OAuth) GetGitHubAccessToken(code string, env *config.Env) (accessToken *st
 	token := parsedQuery["access_token"][0]
 	return &token, nil
 }
+
+// GetGitHubUser is a fucntion to get the GitHub user from the access token provided from github
+func (OAuth) GetGitHubUser(accessToken string) (*schemas.GitHub, error) {
+	req, err := http.NewRequest(http.MethodGet, "https://api.github.com/user", nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", accessToken))
+
+	client := http.Client{
+		Timeout: 30 * time.Second,
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		err = fmt.Errorf("Failed to fetch user data from GitHub")
+		return nil, err
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var payload map[string]interface{}
+	if err = json.Unmarshal(body, &payload); err != nil {
+		return nil, err
+	}
+
+	return &schemas.GitHub{
+		ID:        payload["id"].(float64),
+		Name:      payload["name"].(string),
+		Username:  payload["login"].(string),
+		AvatarURL: payload["avatar_url"].(string),
+		Email:     schemas.GitHub{}.GetEmailFromPayload(payload),
+	}, nil
+}
