369aee62c0d807ea3f70f7bc27d5ce5e0894ce0c
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-07-11 23:28:05 +0530

    Added some needed errors for error handling

--- backend/errors/errors.go
+++ backend/errors/errors.go
@@ -10,17 +10,19 @@ import (
 
 var (
 	//revive:disable
-	ErrInternalServerError      = fmt.Errorf("internal_server_error")
-	ErrUnauthorized             = fmt.Errorf("unauthorized")
-	ErrAccessTokenNotProvided   = fmt.Errorf("access_token_not_provided")
-	ErrBadRequest               = fmt.Errorf("bad_request")
-	ErrIncorrectCredentials     = fmt.Errorf("incorrect_credentials")
-	ErrRefreshTokenExpired      = fmt.Errorf("refresh_token_expired")
-	ErrAccessTokenExpired       = fmt.Errorf("access_token_expired")
-	ErrUsernameAlreadyUsed      = fmt.Errorf("username_already_used")
-	ErrEmailAlreadyUsed         = fmt.Errorf("email_already_used")
-	ErrEmailConfirmationExpired = fmt.Errorf("email_confirmation_expired")
-	Okay                        = "okay"
+	ErrInternalServerError       = fmt.Errorf("internal_server_error")
+	ErrUnauthorized              = fmt.Errorf("unauthorized")
+	ErrAccessTokenNotProvided    = fmt.Errorf("access_token_not_provided")
+	ErrBadRequest                = fmt.Errorf("bad_request")
+	ErrIncorrectCredentials      = fmt.Errorf("incorrect_credentials")
+	ErrRefreshTokenExpired       = fmt.Errorf("refresh_token_expired")
+	ErrAccessTokenExpired        = fmt.Errorf("access_token_expired")
+	ErrUsernameAlreadyUsed       = fmt.Errorf("username_already_used")
+	ErrEmailAlreadyUsed          = fmt.Errorf("email_already_used")
+	ErrEmailConfirmationExpired  = fmt.Errorf("email_confirmation_expired")
+	ErrHaveAnAccountWithTheEmail = fmt.Errorf("already_have_an_account")
+	ErrAddAUsername              = fmt.Errorf("add_a_username")
+	Okay                         = "okay"
 
 //revive:enable
 )
